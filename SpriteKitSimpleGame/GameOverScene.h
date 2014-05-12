9f10ae959adc241dc7b518cce68c54ed59bcb30b
Michael Welburn <mwelbu2@gmail.com>
2014-05-11 21:30:42 -0400

    Completed tutorial

---
+++ SpriteKitSimpleGame/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  SpriteKitSimpleGame
+//
+//  Created by Michael Welburn on 5/11/14.
+//  Copyright (c) 2014 Michael Welburn. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+@end
---
+++ SpriteKitSimpleGame/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  SpriteKitSimpleGame
+//
+//  Created by Michael Welburn on 5/11/14.
+//  Copyright (c) 2014 Michael Welburn. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
